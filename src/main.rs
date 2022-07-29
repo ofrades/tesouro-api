5090e960c88bcd72b2e2cef5f88ab322bb6cbbd5
Miguel Bastos <mig.silva@gmail.com>
2022-07-29 17:04:31 +0100

    add build calendar

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ use chrono::Utc;
 fn main() {
     let today_date = Utc::now();
     println!("{}", today_date.format("%Y-%m-%d"));
+
+    let _calendar = build_calendar(today_date.format("%Y").to_string());
+}
+
+fn build_calendar(year: String) {
+    println!("{}", year)
 }
